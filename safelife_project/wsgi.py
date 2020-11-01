e570ecb1ab69f8495dce3879bb6aa2b91e36b108
itteamforslp <itteamforslp@gmail.com>
2020-10-31 19:18:00 -0700

    Update wsgi.py

--- safelife_project/wsgi.py
+++ safelife_project/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 #sys.path.append('/safelife_project/')
 #sys.path.append('/safelife_project/safelife_project/')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safelife_project.safelife_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safelife_project.settings')
 #import django.core.handlers.wsgi
 #application = django.core.handlers.wsgi.WSGIHandler()
 application = get_wsgi_application()

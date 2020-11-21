3dc4a36c85fee8b86d29d2494690a411265a9d65
itteamforslp <itteamforslp@gmail.com>
2020-11-20 19:02:21 -0800

    changed staticfiles to static

--- safelife_project/settings.py
+++ safelife_project/settings.py
@@ -20,7 +20,7 @@ import os.path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.

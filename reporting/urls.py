9febf03f39c7da1bef8c8d53e4b1635af2d50da1
Christopher Gomes <chrisgomes64@gmail.com>
2021-01-02 11:12:00 -0800

    Update urls.py

--- reporting/urls.py
+++ reporting/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('<int:course_id>/', views.report, name ='reports'),
-    path('<int:course_id>/0/', views.report, name='reports'),
+    path('<int:course_id>/0', views.report, name='reports'),
     path('<int:course_id>/<int:month>/', views.report, name='reports'),
     path('', views.index, name='index'),
     path('', views.report, name='teacher-reporting'),

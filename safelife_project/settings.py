714f091b5afc002153622b7827c6acf4e98d8263
itteamforslp <itteamforslp@gmail.com>
2021-08-29 14:05:59 -0700

    Update settings.py

--- safelife_project/settings.py
+++ safelife_project/settings.py
@@ -38,7 +38,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(‘SECRET_KEY’)
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

5440b789b5b66c4cb09c7e32d146f129c9690843
Christopher Gomes <chrisgomes64@gmail.com>
2021-08-30 16:27:52 -0700

    env update

--- safelife_project/settings.py
+++ safelife_project/settings.py
@@ -98,10 +98,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'itteamforslp@gmail.com' 
-EMAIL_HOST_PASSWORD = 'slpgoliveready' 
-DEFAULT_FROM_EMAIL = 'itteamforslp@gmail.com'
+EMAIL_PORT = env('EMAIL_PORT') 
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD') 
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 
 
